8c14c2b91df442aaf5e07d3d9efd78bb26879eee
Gladwin A <gladwina72@gmail.com>
2026-07-20 13:17:10 -0500

    Last import

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Simon-Advantage";
+  public static final String MAVEN_NAME = "Simon";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-07-20 13:13:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1784567614564L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "e543a7d80fad96c55a599903be377b71549d1d5b";
+  public static final String GIT_DATE = "2026-07-20 14:09:32 EDT";
+  public static final String GIT_BRANCH = "AdvantageKit";
+  public static final String BUILD_DATE = "2026-07-20 14:15:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1784571344673L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
